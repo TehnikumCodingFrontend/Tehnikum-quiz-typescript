b4dc19b99e454140ffecfb2b464b7a1ce0ded732
TEHNIKUM CODING (frontend) <142232787+TehnikumCodingFrontend@users.noreply.github.com>
2024-02-04 18:53:04 +0500

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import "./App.css";
-import StepFour from "./pages/StepFour";
-import StepOne from "./pages/StepOne";
-import StepThree from "./pages/StepThree";
-import StepTwo from "./pages/StepTwo";
-import Thanks from "./pages/Thanks";
 import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <div className="App">
       <Welcome />
-      <StepOne/>
-      <StepTwo/>
-      <StepThree/>
-      <StepFour/>
-      <Thanks/>
     </div>
   );
 }
