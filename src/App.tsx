d748726c88ee70d43db00eba8279017dd9a32e65
Eva Corp <142232787+TehnikumCodingFrontend@users.noreply.github.com>
2024-03-03 15:18:35 +0500

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import "./App.css";
-import StepFour from "./pages/StepFour";
-import StepOne from "./pages/StepOne";
-import StepThree from "./pages/StepThree";
-import StepTwo from "./pages/StepTwo";
-import Thanks from "./pages/Thanks";
 import Welcome from "./pages/Welcome";
-import { YourAnswers } from "./pages/YourAnswers";
 
 function App() {
   return (
     <div className="App">
       <Welcome />
-      <StepOne/>
-      <StepTwo/>
-      <StepThree/>
-      <StepFour/>
-      <YourAnswers/>
-      <Thanks/>
     </div>
   );
 }
